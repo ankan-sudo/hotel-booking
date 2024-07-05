307089f31999241d9ab145df9eaf652ac512be3a
Ankan Chanda <ankanchanda11@gmail.com>
2024-07-05 12:03:34 +0530

    first commit

---
+++ client/src/pages/ProfilePage.jsx
@@ -0,0 +1,55 @@
+import { useContext, useState } from "react"
+import { UserContext } from "../UserContext.jsx"
+import { Navigate, useParams } from "react-router-dom";
+import { Link , useLocation} from "react-router-dom";
+import axios from "axios";
+import PlacesPage from "./PlacesPage.jsx";
+import AccountNav from "../AccountNav.jsx";
+
+
+const ProfilePage = () => {
+  const {ready, user, setUser} = useContext(UserContext);
+  const [redirect, setRedirect] = useState(null);
+
+  const { pathname } = useLocation();
+  let subpage = pathname.split('/')?.[2];
+
+  if (subpage === undefined) {
+    subpage = 'profile';
+  }
+
+  async function logout() {
+    await axios.post('/api/logout'); // Corrected route path
+    setRedirect('/');
+    setUser(null);
+  }
+
+  if (!ready) {
+    return 'Loading...';
+  }
+  if (ready && !user && !redirect) {
+    return <Navigate to={'/login'} />;
+  }
+
+  if (redirect) {
+    return <Navigate to={redirect} />;
+  }
+
+  return (
+    <div>
+      <AccountNav />
+      {subpage === 'profile' && (
+        <div className='text-center max-w-lg mx-auto'>
+          Logged in as {user.name} ({user.email}) <br />
+          <button onClick={logout} className='primary max-w-sm mt-2'>
+            Logout
+          </button>
+        </div>
+      )}
+
+      {subpage === 'places' && <PlacesPage />}
+    </div>
+  );
+};
+
+export default ProfilePage;
